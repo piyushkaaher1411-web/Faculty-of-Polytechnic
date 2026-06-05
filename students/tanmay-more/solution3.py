af8febf3b5a11519e5d56cddceb6caa1b756f81c
tanmaymore1705-dotcom <tanmaymore1705@gmail.com>
2026-06-05 11:20:47 +0530

    Add vowel counting logic in solution3.py
    Count the number of vowels in a predefined set of characters.

--- students/tanmay-more/solution3.py
+++ students/tanmay-more/solution3.py
@@ -1,1 +1,17 @@
-
+#pratical no 8
+s={"a", "b", "c", "d", "e", "f", "g", "h","i","j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+count=0
+for x in s:
+    if x=="a":
+        count=count+1
+    elif x=="e": 
+        count=count+1
+    elif x=="i":
+        count=count+1
+    elif x=="o":
+        count=count+1
+    elif x=="u":
+        count=count+1
+    else :
+        continue 
+print (count)
