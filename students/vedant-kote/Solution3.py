e136b44a934ce4595698719d457a09f25046abe3
kotevedant8-gif <kotevedant8@gmail.com>
2026-06-08 10:39:15 +0530

    Update Solution3.py

--- students/vedant-kote/Solution3.py
+++ students/vedant-kote/Solution3.py
@@ -35,3 +35,20 @@ for x in s:
     else:
         continue 
 print(count)
+
+#problem no5
+s = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",  "k", "l", "m", "n", "o", "p", "q", "r", "s",  "t", "u", "v", "w", "x", "y", "z"}
+
+v = 0
+c= 0
+
+vowels = "aeiou"
+
+for x in s:
+    if x in vowels:
+        v = v + 1  
+    else:
+        c = c+ 1  
+
+print("Total Vowels:", v)
+print("Total Consonants:", c)
